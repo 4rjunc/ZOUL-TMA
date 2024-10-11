4bffc8ba746f6b6132cc71c6024d639729b0e58c
4rjunc <arjunmundamani@gmail.com>
2024-10-11 20:22:46 +0530

    Updated the path

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: "docs",
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  base: process.env.GH_PAGES ? "/ZOUL-TMA/" : "./",
   server: {
     fs: {
-      allow: ['../sdk', './'],
+      allow: ["../sdk", "./"],
     },
   },
-})
+});
