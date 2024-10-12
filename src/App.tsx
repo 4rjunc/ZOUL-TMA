c6035df7274b13af65fc7c8568d00cd2c23439e3
4rjunc <arjunmundamani@gmail.com>
2024-10-12 11:15:20 +0530

    Added Browser Routes🛠

--- src/App.tsx
+++ src/App.tsx
@@ -6,116 +6,149 @@ import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { TxForm } from "./components/TxForm/TxForm";
 import { Calls } from "./components/Calls/Calls";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
+const HomePage = () => (
+  <div>
+    <Home />
+  </div>
+);
+const CallPage = () => (
+  <div>
+    <Calls />
+  </div>
+);
+const ProfilePage = () => (
+  <div>
+    <TxForm />
+  </div>
+);
+
+const AppContent = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calls" element={<CallPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        {/*<TonProofDemo />*/}
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 function App() {
   return (
-    <TonConnectUIProvider
-      manifestUrl="https://4rjunc.github.io/ZOUL-TMA/tonconnect-manifest.json"
-      uiPreferences={{ theme: THEME.DARK }}
-      walletsListConfiguration={{
-        includeWallets: [
-          {
-            appName: "tonwallet",
-            name: "TON Wallet",
-            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
-            aboutUrl:
-              "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
-            universalLink: "https://wallet.ton.org/ton-connect",
-            jsBridgeKey: "tonwallet",
-            bridgeUrl: "https://bridge.tonapi.io/bridge",
-            platforms: ["chrome", "android"],
-          },
-          {
-            appName: "nicegramWallet",
-            name: "Nicegram Wallet",
-            imageUrl: "https://static.nicegram.app/icon.png",
-            aboutUrl: "https://nicegram.app",
-            universalLink: "https://nicegram.app/tc",
-            deepLink: "nicegram-tc://",
-            jsBridgeKey: "nicegramWallet",
-            bridgeUrl: "https://bridge.tonapi.io/bridge",
-            platforms: ["ios", "android"],
-          },
-          {
-            appName: "fintopio-tg",
-            name: "Fintopio Telegram",
-            imageUrl: "https://fintopio.com/tonconnect-icon.png",
-            aboutUrl: "https://fintopio.com",
-            universalLink: "https://t.me/fintopio?attach=wallet",
-            bridgeUrl: "https://wallet-bridge.fintopio.com/bridge",
-            platforms: ["ios", "android", "macos", "windows", "linux"],
-          },
-          {
-            appName: "tokenpocket",
-            name: "TokenPocket",
-            imageUrl: "https://hk.tpstatic.net/logo/tokenpocket.png",
-            aboutUrl: "https://tokenpocket.pro",
-            jsBridgeKey: "tokenpocket",
-            platforms: ["ios", "android"],
-          },
-          {
-            appName: "dewallet",
-            name: "DeWallet",
-            imageUrl:
-              "https://raw.githubusercontent.com/delab-team/manifests-images/main/WalletAvatar.png",
-            aboutUrl: "https://delabwallet.com",
-            universalLink: "https://t.me/dewallet?attach=wallet",
-            bridgeUrl: "https://bridge.dewallet.pro/bridge",
-            platforms: ["ios", "android", "macos", "windows", "linux"],
-          },
-          {
-            appName: "BitgetWeb3",
-            name: "BitgetWeb3",
-            imageUrl: "https://img.bitgetimg.com/image/third/1723701408284.png",
-            aboutUrl: "https://www.bitget.com",
-            universalLink: "https://t.me/BitgetOfficialBot?attach=wallet",
-            bridgeUrl: "https://ton-connect-bridge.bgwapi.io/bridge",
-            platforms: ["ios", "android", "windows", "macos", "linux"],
-          },
-          {
-            appName: "cdcTonWallet",
-            name: "Crypto.com DeFi Wallet",
-            imageUrl: "https://apro-ncw-api-file.crypto.com/wallet/logo",
-            aboutUrl: "https://crypto.com/defi-wallet",
-            universalLink: "https://wallet.crypto.com/deeplink/ton-connect",
-            deepLink: "dfw://",
-            jsBridgeKey: "cdcTonWallet",
-            bridgeUrl: "https://wallet.crypto.com/sse/tonbridge",
-            platforms: ["ios", "android", "chrome"],
-          },
-          {
-            appName: "tobi",
-            name: "Tobi",
-            imageUrl: "https://app.tobiwallet.app/icons/logo.png",
-            aboutUrl: "https://tobi.fun",
-            universalLink: "https://t.me/TobiWalletBot?attach=wallet",
-            bridgeUrl: "https://bridge.tonapi.io/bridge",
-            platforms: ["ios", "android", "macos", "windows", "linux"],
-          },
-          {
-            appName: "trustwalletTon",
-            name: "Trust",
-            imageUrl: "https://assets-cdn.trustwallet.com/dapps/trust.logo.png",
-            aboutUrl: "https://trustwallet.com/about-us",
-            bridgeUrl: "https://tonconnect.trustwallet.com/",
-            jsBridgeKey: "trustwalletTon",
-            platforms: ["chrome", "ios", "android"],
-          },
-        ],
-      }}
-      actionsConfiguration={{
-        twaReturnUrl: "https://t.me/zoulhelper_bot/start",
-      }}
-    >
-      <div className="app">
-        <Header />
-        <Home />
-        <Calls />
-        <TxForm />
-        {/*<TonProofDemo />*/}
-        <Footer />
-      </div>
-    </TonConnectUIProvider>
+    <Router>
+      <TonConnectUIProvider
+        manifestUrl="https://4rjunc.github.io/ZOUL-TMA/tonconnect-manifest.json"
+        uiPreferences={{ theme: THEME.DARK }}
+        walletsListConfiguration={{
+          includeWallets: [
+            {
+              appName: "tonwallet",
+              name: "TON Wallet",
+              imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+              aboutUrl:
+                "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+              universalLink: "https://wallet.ton.org/ton-connect",
+              jsBridgeKey: "tonwallet",
+              bridgeUrl: "https://bridge.tonapi.io/bridge",
+              platforms: ["chrome", "android"],
+            },
+            {
+              appName: "nicegramWallet",
+              name: "Nicegram Wallet",
+              imageUrl: "https://static.nicegram.app/icon.png",
+              aboutUrl: "https://nicegram.app",
+              universalLink: "https://nicegram.app/tc",
+              deepLink: "nicegram-tc://",
+              jsBridgeKey: "nicegramWallet",
+              bridgeUrl: "https://bridge.tonapi.io/bridge",
+              platforms: ["ios", "android"],
+            },
+            {
+              appName: "fintopio-tg",
+              name: "Fintopio Telegram",
+              imageUrl: "https://fintopio.com/tonconnect-icon.png",
+              aboutUrl: "https://fintopio.com",
+              universalLink: "https://t.me/fintopio?attach=wallet",
+              bridgeUrl: "https://wallet-bridge.fintopio.com/bridge",
+              platforms: ["ios", "android", "macos", "windows", "linux"],
+            },
+            {
+              appName: "tokenpocket",
+              name: "TokenPocket",
+              imageUrl: "https://hk.tpstatic.net/logo/tokenpocket.png",
+              aboutUrl: "https://tokenpocket.pro",
+              jsBridgeKey: "tokenpocket",
+              platforms: ["ios", "android"],
+            },
+            {
+              appName: "dewallet",
+              name: "DeWallet",
+              imageUrl:
+                "https://raw.githubusercontent.com/delab-team/manifests-images/main/WalletAvatar.png",
+              aboutUrl: "https://delabwallet.com",
+              universalLink: "https://t.me/dewallet?attach=wallet",
+              bridgeUrl: "https://bridge.dewallet.pro/bridge",
+              platforms: ["ios", "android", "macos", "windows", "linux"],
+            },
+            {
+              appName: "BitgetWeb3",
+              name: "BitgetWeb3",
+              imageUrl:
+                "https://img.bitgetimg.com/image/third/1723701408284.png",
+              aboutUrl: "https://www.bitget.com",
+              universalLink: "https://t.me/BitgetOfficialBot?attach=wallet",
+              bridgeUrl: "https://ton-connect-bridge.bgwapi.io/bridge",
+              platforms: ["ios", "android", "windows", "macos", "linux"],
+            },
+            {
+              appName: "cdcTonWallet",
+              name: "Crypto.com DeFi Wallet",
+              imageUrl: "https://apro-ncw-api-file.crypto.com/wallet/logo",
+              aboutUrl: "https://crypto.com/defi-wallet",
+              universalLink: "https://wallet.crypto.com/deeplink/ton-connect",
+              deepLink: "dfw://",
+              jsBridgeKey: "cdcTonWallet",
+              bridgeUrl: "https://wallet.crypto.com/sse/tonbridge",
+              platforms: ["ios", "android", "chrome"],
+            },
+            {
+              appName: "tobi",
+              name: "Tobi",
+              imageUrl: "https://app.tobiwallet.app/icons/logo.png",
+              aboutUrl: "https://tobi.fun",
+              universalLink: "https://t.me/TobiWalletBot?attach=wallet",
+              bridgeUrl: "https://bridge.tonapi.io/bridge",
+              platforms: ["ios", "android", "macos", "windows", "linux"],
+            },
+            {
+              appName: "trustwalletTon",
+              name: "Trust",
+              imageUrl:
+                "https://assets-cdn.trustwallet.com/dapps/trust.logo.png",
+              aboutUrl: "https://trustwallet.com/about-us",
+              bridgeUrl: "https://tonconnect.trustwallet.com/",
+              jsBridgeKey: "trustwalletTon",
+              platforms: ["chrome", "ios", "android"],
+            },
+          ],
+        }}
+        actionsConfiguration={{
+          twaReturnUrl: "https://t.me/zoulhelper_bot/start",
+        }}
+      >
+        <AppContent />
+      </TonConnectUIProvider>
+    </Router>
   );
 }
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,31 @@
 import { Tabbar } from "@telegram-apps/telegram-ui";
 import { Icon24User, Icon24Home, Icon24Sparkle } from "@vkontakte/icons";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./footer.scss";
 import { useState } from "react";
 
 export const Footer = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const tabs = [
-    { id: "home", text: "Home", Icon: Icon24Home },
-    { id: "calls", text: "Calls", Icon: Icon24Sparkle },
-    { id: "profile", text: "Profile", Icon: Icon24User },
+    { id: "home", text: "Home", Icon: Icon24Home, path: "/" },
+    { id: "calls", text: "Calls", Icon: Icon24Sparkle, path: "/calls" },
+    { id: "profile", text: "Profile", Icon: Icon24User, path: "/profile" },
   ];
   const [currentTab, setCurrentTab] = useState(tabs[0].id);
 
   return (
     <footer className="footer">
       <Tabbar>
-        {tabs.map(({ id, text, Icon }) => (
+        {tabs.map(({ id, text, Icon, path }) => (
           <Tabbar.Item
             key={id}
             text={text}
             selected={id === currentTab}
-            onClick={() => setCurrentTab(id)}
+            onClick={() => {
+              setCurrentTab(id);
+              navigate(path);
+            }}
           >
             <Icon />
           </Tabbar.Item>
